525d91212acc98ca74b74e3897a776801f96a6bb
samikmaharjan55 <samikmaharjan55@gmail.com>
2024-06-27 15:22:57 +0545

    News Api

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,13 +4,19 @@ import NewsItem from "./NewsItem";
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component.
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className="container my-3">
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="MyTitle" descriptio="MyDescription" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="MyTitle" descriptio="MyDescription" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="MyTitle" descriptio="MyDescription" />
+          </div>
+        </div>
       </div>
     );
   }
