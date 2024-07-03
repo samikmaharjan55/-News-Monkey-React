9020b63c77adf1919e8ea5fcb15f2505f66686b3
samikmaharjan55 <samikmaharjan55@gmail.com>
2024-07-03 17:19:39 +0545

    Part 28 Complete

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,26 +1,27 @@
-import axios from "axios";
-import React from "react";
+// import axios from "axios";
+// import React from "react";
 
-const Test = () => {
-  async function handlePost() {
-    const response = await axios
-      .post("https://dummyjson.com/auth/login", {
-        username: "emilys",
-        password: "emilyspass",
-        expiresInMins: 30,
-      })
-      .then((res) => res.status);
-    if (response === 200) {
-      console.log("sucess");
-    } else {
-      console.log("failed");
-    }
-  }
-  return (
-    <div>
-      <button onClick={handlePost}>post</button>
-    </div>
-  );
-};
+// const Test = () => {
+//   // let data = await axios.get(url).then((response) => response.data);
+//   async function handlePost() {
+//     const response = await axios
+//       .post("https://dummyjson.com/auth/login", {
+//         username: "emilys",
+//         password: "emilyspass",
+//         expiresInMins: 30,
+//       })
+//       .then((res) => res.status);
+//     if (response === 200) {
+//       console.log("sucess");
+//     } else {
+//       console.log("failed");
+//     }
+//   }
+//   return (
+//     <div>
+//       <button onClick={handlePost}>post</button>
+//     </div>
+//   );
+// };
 
-export default Test;
+// export default Test;
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
-import axios from "axios";
 
 export class News extends Component {
   constructor() {
@@ -8,20 +7,50 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
+      page: 1,
     };
   }
 
   async componentDidMount() {
     let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=83ce76eddf244d31b2444b991092684d";
-    // let data = await fetch(url);
-    // let parsedData = await data.json();
-    // console.log(parsedData);
-    // this.setState({ articles: parsedData.articles });
-    let data = await axios.get(url).then((response) => response.data);
-    console.log(data);
-    this.setState({ articles: data.articles });
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=83ce76eddf244d31b2444b991092684d&page=1&pageSize=20";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
+    // let data = await axios.get(url).then((response) => response.data);
+    // console.log(data);
+    // this.setState({ articles: data.articles });
   }
+
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=83ce76eddf244d31b2444b991092684d&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page - 1,
+    });
+  };
+
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=83ce76eddf244d31b2444b991092684d&page=${
+        this.state.page + 1
+      }&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        articles: parsedData.articles,
+        page: this.state.page + 1,
+      });
+    }
+  };
 
   render() {
     return (
@@ -42,6 +71,22 @@ export class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}>
+            &larr; Previous
+          </button>
+          <h4>{this.state.page}</h4>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}>
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
